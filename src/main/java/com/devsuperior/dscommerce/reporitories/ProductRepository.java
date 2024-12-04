bc531ad13e505df230601bda5f23eda0b3f489a9
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-12-04 16:13:06 -0300

    Criação do ProductRepository para teste

---
+++ src/main/java/com/devsuperior/dscommerce/reporitories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dscommerce.reporitories;
+
+import com.devsuperior.dscommerce.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository <Product, Long>{
+
+
+
+
+
+}
