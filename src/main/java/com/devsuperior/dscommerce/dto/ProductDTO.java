3d9d9effb70d8a93579ad00de70f317056ba64ec
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-12-16 13:59:04 -0300

    Validação com bean Validation

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -1,13 +1,22 @@
 package com.devsuperior.dscommerce.dto;
 
 import com.devsuperior.dscommerce.entities.Product;
+import jakarta.validation.constraints.*;
 
 public class ProductDTO {
 
 
     private Long id;
+
+    @Size(min = 3, max = 80, message = "Nome deve ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+
+    @Size(min = 10, message = "Descrição deve ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
